9253c7145b1ff184a8b3c29a2495c8446921bf6e
Phuc <mphuc1010@gmail.comm>
2024-07-09 16:21:07 +0700

    chore: Update MongoDB and Mongoose dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,47 @@
-import express from "express";
-import authControllers from "./controllers/authControllers";
-import postControllers from "./controllers/postControllers";
+// import express from "express";
+// import authControllers from "./controllers/authControllers";
+// import postControllers from "./controllers/postControllers";
 
-const app = express();
+// const app = express();
 
-app.use("/auth", authControllers);
-app.use("/posts", postControllers);
+// app.use("/auth", authControllers);
+// app.use("/posts", postControllers);
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!!!");
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://mphuc1010:123456Aa@cluster0.jmyiqzw.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
